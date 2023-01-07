d2fd9cb1c44f207de95d9ad5f7494e4bf302ad0e
ASaidOguz <ahmetsaid2136@gmail.com>
2023-01-07 23:48:09 +0300

    set api

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL:"https://express-server-nc6y-git-master-asaidoguz.vercel.app/",
+  baseURL:"https://express-server-virid.vercel.app",
 });
 
 export default server;
